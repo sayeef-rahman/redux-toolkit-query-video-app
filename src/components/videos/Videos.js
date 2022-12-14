3dc89f9dad927ff42cadc2c92b3984a451813695
sayeef-rahman <sayeef.cse@gmail.com>
2022-12-15 01:12:15 +0600

    homepage videos section complete

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,12 +1,35 @@
 import { useGetVideosQuery } from "../../features/api/apiSlice";
 import Video from "./Video";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
 
 export default function Videos() {
-    const {data} = useGetVideosQuery();
-    console.log(data);
-    return (
-        <>
-            <Video />
-        </>
+  const { data: videos, isError, isFetching, isLoading, isSuccess, error } =
+    useGetVideosQuery();
+
+  let content = null;
+  if (isLoading) {
+    content = (
+      <>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+      </>
     );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="There was an error" />;
+  }
+
+  if (!isLoading && !isError && videos?.length === 0) {
+    content = <Error message="No videos found!" />;
+  }
+
+  if (!isLoading && !isError && videos?.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
+
+  return content;
 }
