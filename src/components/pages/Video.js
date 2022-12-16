ba8889fd6d31616f06ad074c4bf6b533219c4b40
sayeef-rahman <sayeef.cse@gmail.com>
2022-12-17 00:16:12 +0600

    single video with dynamic route & api complted

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,20 +1,55 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
+import PlayerLoader from "../../components/ui/loaders/PlayerLoader";
+import DescriptionLoader from "../../components/ui/loaders/DescriptionLoader";
+import RelatedVideoLoader from "../../components/ui/loaders/RelatedVideoLoader";
 
 export default function Video() {
-    return (
-        <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
-            <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                <div className="grid grid-cols-3 gap-2 lg:gap-8">
-                    <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
-                    </div>
+  const { videoId } = useParams();
+  const { data: video, isLoading, isError, error } = useGetVideoQuery(videoId);
+  console.log("video: ", video);
 
-                    <RelatedVideos />
-                </div>
-            </div>
-        </section>
+  let content = null;
+  if (isLoading || !video) {
+    content = (
+      <>
+        <PlayerLoader />
+        <DescriptionLoader />
+      </>
     );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="There was an error" />;
+  }
+
+  if (!isLoading && !isError && video?.id) {
+    content = (
+      <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
+        <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+          <div className="grid grid-cols-3 gap-2 lg:gap-8">
+            <div className="col-span-full w-full space-y-8 lg:col-span-2">
+              <Player link={video.link} title={video.title} />
+              <Description  video={video}  />
+            </div>
+            {video?.id ? (
+              <RelatedVideos />
+            ) : isLoading ? (
+              <>
+                <RelatedVideoLoader /> <RelatedVideoLoader />{" "}
+                <RelatedVideoLoader />
+              </>
+            ) : (
+              <Error message="There was an Error!" />
+            )}
+          </div>
+        </div>
+      </section>
+    );
+  }
+  return content;
 }
--- src/components/videos/Video.js
+++ src/components/videos/Video.js
@@ -7,7 +7,7 @@ export default function Video({ video }) {
     <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
         <div className="relative">
-          <Link to="/videos/1">
+          <Link to={`/videos/${id}`}>
             <img
               src={thumbnail}
               className="w-full h-auto"
